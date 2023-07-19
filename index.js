95142bb7642fcefbd78e2ab95af4f842f007ac6e
Vincent Delitz <vincent.delitz@tum.de>
2023-07-19 20:08:18 +0200

    more refactoring

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 require("dotenv").config();
 
-const { ms } = require("@simpleanalytics/common");
+const {ms} = require("@simpleanalytics/common");
 const {email} = require("./lib/email")
 const crawlers = require("./crawlers/index");
 const notify = require("./lib/notify");
-const { loop } = require("./lib/utils");
+const {loop} = require("./lib/utils");
 
-const { NODE_ENV = "production" } = process.env;
+const {NODE_ENV = "production"} = process.env;
 
 (async () => {
-  if (NODE_ENV === "production") {
-    let message = "News alerts app just started up";
-    try {
-      const current = require("./commits/current.json");
-      const previous = require("./commits/previous.json");
+    if (NODE_ENV === "production") {
+        let message = "News alerts app just started up";
 
-      const url =
-        previous.commit && current.commit
-          ? `https://github.com/${current.repo}/compare/${previous.commit}...${current.commit}`
-          : null;
 
-      const commit = current?.message?.replace(/-/g, " ");
-      message = `Deployed "${commit}" ${
-        url ? `<a href="${url}">GitHub</a>` : ""
-      }`;
-    } catch (error) {
-      console.log(error);
+        console.log("TESTING -----")
+        //Send message via email
+        await email("Newsbot started", "Newsbot started up successfully")
+        console.log("IS THIS SKIPPED?")
+        //sendmessage(message, { silent: true });
     }
 
-    console.log("TESTING -----")
-     //Send message via email
-    await email("Newsbot started", "Newsbot started up successfully")
-    console.log("IS THIS SKIPPED?")
-    //sendmessage(message, { silent: true });
-  }
-
-  loop(crawlers, { interval: ms.second * 90 });
-  loop(notify, { interval: ms.second * 30 });
+    loop(crawlers, {interval: ms.second * 90});
+    loop(notify, {interval: ms.second * 30});
 })();
