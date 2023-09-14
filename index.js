7675190845117ce85fbf9127f014fec0675434fe
Vincent Delitz <vincent.delitz@corbado.com>
2023-09-14 14:16:01 +0200

    some refactoring

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ const {NODE_ENV = "production"} = process.env;
 (async () => {
     // set NODE_ENV on production to start app
     if (NODE_ENV === "production") {
-
-        let message = "News alerts app just started up";
-        console.log(message)
+        console.log("News alerts app just started up")
         
         //Send message via email
         await email("Newsbot started", "Newsbot started up successfully")
 
     }
-
 
     loop(crawlers, {interval: ms.second * 90}); // every 90 seconds, crawl hackernews and googlealerts
     loop(notify, {interval: ms.second * 30}); // every 30 seconds, check if there is new article which needs to be notified of
